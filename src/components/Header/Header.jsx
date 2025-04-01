f9464c30c5852d67ddd0c11eb4597430a03bd38e
Modesto <mofesdo.dev@gmail.com>
2025-04-01 04:39:31 -0700

    Feat: Links created for navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,32 @@
 import "./Header.css";
 import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
 
-function Header(){
-    return (
-        <header className="header">
-            <img src={logo} alt="" className="header__logo" />
-            <h1 className="header__title">PokeString</h1>
-            <ul className="header__container">
-                <li className="header__item"><a href="" className="header__link">Events</a></li>
-                <li className="header__item"><a href="" className="header__link">Create</a></li>
-                <li className="header__item"><a href="" className="header__link">Popular</a></li>
-            </ul>
-        </header>
-    );
+function Header() {
+  return (
+    <header className="header">
+      <Link to="/" className="header__link">
+        <img src={logo} alt="" className="header__logo" />
+      </Link>
+      <h1 className="header__title">PokeString</h1>
+      <ul className="header__container">
+        <Link to="/events" className="header__link">
+          <li className="header__item">Events</li>
+        </Link>
+
+        <li className="header__item">
+          <a href="" className="header__link">
+            Create
+          </a>
+        </li>
+        <li className="header__item">
+          <a href="" className="header__link">
+            Popular
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
 }
 
 export default Header;
