e66d867424bbbb305f56835929540d66bac9d20e
Modesto <mofesdo.dev@gmail.com>
2025-04-04 02:13:29 -0700

    Feat: Can now copy event search string with button

--- src/components/Evts/Evts.jsx
+++ src/components/Evts/Evts.jsx
@@ -1,10 +1,12 @@
-import "./Evts.css"
+import "./Evts.css";
 import PkmnCard from "../PkmnCard/PkmnCard";
 import { fetchSpawnsData } from "../../utils/api";
 import { useEffect, useState } from "react";
 
 function Evts() {
   const eventSpawnNums = [351, 10028, 10029, 10030, 412, 10034, 10035];
+  const spawnString = eventSpawnNums.filter((num) => num < 1020).toString();
+  console.log(spawnString);
   const [eventSpawns, setEventSpawns] = useState([]);
 
   useEffect(() => {
@@ -16,16 +18,23 @@ function Evts() {
       })
       .catch((error) => console.error("Operation failed:", error));
   }, []);
-
-  console.log(eventSpawns);
+  const copyString = () => {
+    navigator.clipboard.writeText(spawnString);
+  };
   return (
     <div className="events">
+      <h2 className="events__heading">Wild Spawns</h2>
       <ul className="pokemon__list">
         {eventSpawns.map((pkmn) => (
-          <PkmnCard key={pkmn.id} pkmn={pkmn}/>
+          <PkmnCard key={pkmn.id} pkmn={pkmn} />
         ))}
       </ul>
-      <h2>test</h2>
+      <div className="event__copy-container">
+        <p className="events__string">Search String: {spawnString} </p>
+        <button className="events__copy-btn" onClick={copyString}>
+          Copy to Clipboard
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/PkmnCard/PkmnCard.jsx
+++ src/components/PkmnCard/PkmnCard.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 import "./PkmnCard.css"
 function PkmnCard({pkmn}){
-    console.log(pkmn);
+
     return (
         <li className="pkmn-card" key={pkmn.id}>
             <h2 className="pkmn-card__name">{pkmn.name}</h2>
             <img src={pkmn.sprites.front_default} alt="" className="pkmn-card__sprite" />
-            <p className="pkmn-card__id"># {pkmn.id}</p>
+            {/* <p className="pkmn-card__id"># {pkmn.id}</p> */}
         </li>
     );
 }
