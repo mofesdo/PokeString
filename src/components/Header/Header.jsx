b2ad455314eee456d58cf7d5f06b47dea7863032
Modesto <mofesdo.dev@gmail.com>
2025-04-08 05:30:11 -0700

    Feat: Added a collapsable hamburger menu to header links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
 import "./Header.css";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    console.log("clicked");
+    setIsOpen(!isOpen);
+  };
   return (
     <header className="header">
       <Link to="/" className="header__link">
@@ -10,7 +17,8 @@ function Header() {
       </Link>
       <h1 className="header__title">PokeString</h1>
       <h2 className="header__beta">Beta</h2>
-      <ul className="header__container">
+      <button className="header__menu" onClick={toggleMenu}>&#9776;</button>
+      <ul className={`header__container ${isOpen ? "active" : ""}`}>
         <Link to="/events" className="header__link">
           <li className="header__item">Events</li>
         </Link>
